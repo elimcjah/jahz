329ec0dc37edd40ad770f94d12438905d4353457
Elijah McClendon <elijah.mcclendon@vgw.co>
2020-05-03 05:52:28 -0600

    dang.  Just wanna commit without an error.

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    'prettier',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+    'plugin:nuxt/recommended'
+  ],
+  ignorePatterns: ['nuxt.config.js', 'node_modules/'],
+  plugins: ['prettier'],
+  // add your custom rules here
+  rules: {}
+}
---
+++ pages/stack.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Bell Div Div OOOOOO
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Stack'
+}
+</script>
+
+<style scoped></style>
